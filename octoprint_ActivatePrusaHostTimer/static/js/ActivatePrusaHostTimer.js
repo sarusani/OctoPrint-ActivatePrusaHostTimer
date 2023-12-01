feab554ed701e23ceb3058d6b269dbb12708efb6
sarusani <62053849+sarusani@users.noreply.github.com>
2023-12-01 12:05:19 +0100

    Send gcode to printer

--- octoprint_ActivatePrusaHostTimer/static/js/ActivatePrusaHostTimer.js
+++ octoprint_ActivatePrusaHostTimer/static/js/ActivatePrusaHostTimer.js
@@ -5,22 +5,14 @@
  * License: AGPLv3
  */
 $(function() {
-    function ActivatePrusaHostTimerViewModel(parameters) {
+    function ActivatePrusaHostTimerViewModel() {
         var self = this;
-        self.terminal = parameters[0];
-
         self.commandString = 'M79 S"OP"'
 
-        self.terminal.command(self.commandString);        
-
         var intervalId = window.setInterval(function(){
-            self.terminal.sendCommand();
+            OctoPrint.control.sendGcode(self.commandString);
         }, 10000);
     }
 
-    // view model class, parameters for constructor, container to bind to
-    OCTOPRINT_VIEWMODELS.push([
-        ActivatePrusaHostTimerViewModel,
-        ["terminalViewModel"],
-    ]);
+    ActivatePrusaHostTimerViewModel()
 });
