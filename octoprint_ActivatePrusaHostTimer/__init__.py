53e1a2a93919dd2beaf772c916c52157ec51331e
sarusani <github@bigtree.ch>
2024-07-30 17:25:33 +0200

    Fix whitespace removal
    Remove whitespaces when filename is longer than 15 chars.

--- octoprint_ActivatePrusaHostTimer/__init__.py
+++ octoprint_ActivatePrusaHostTimer/__init__.py
@@ -142,8 +142,8 @@ class ActivatePrusaHostTimerPlugin(
 		#Do not show a notification or start a print if the printer is not ready.
 		if self._printer.is_ready():
 			self._showNotification("Printer is ready. Printing: %s" % (currentJobName))
-			#Remove all whitespaces from string if longer than 20 characters.
-			lcdMessage = currentJobName if len(currentJobName) <= 20 else ''.join(currentJobName.split())
+			#Remove all whitespaces from string if longer than 15 characters.
+			lcdMessage = currentJobName if len(currentJobName) <= 15 else ''.join(currentJobName.split())
 			self._showLCDNotification("File:%s" % (lcdMessage))
 			
 			#Make sure state didn't change while sending messages
